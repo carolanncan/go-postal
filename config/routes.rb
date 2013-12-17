9b7b5a9c5a0073738fe64588f9755e9264fea59b
Carolann Bonner <carolanncan@gmail.com>
2013-12-17 10:34:17 -0800

    Adds all pages into views. Adds partial to form pages.

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ LetterWritingRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   #  root 'welcome#index'
-   root to: 'homes#index'
+  root to: 'homes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   #   get '/write' => 'homes#write'
-get "static_pages/write"
-
+  get "static_pages/write"
+  get "static_pages/address"
+  get "static_pages/send"
+  get "static_pages/confirmation"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
