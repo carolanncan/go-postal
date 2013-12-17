8b637f6305b12a9c2c4ffd73da9e3e7b2af0c083
Carolann Bonner <carolanncan@gmail.com>
2013-12-16 20:12:16 -0800

    First commit. Sets up the project. Adds views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LetterWritingRails::Application.config.secret_key_base = '4a9532eea09836ab87d2dc998856aac2216a5f66fd09fac241a3c5c9e0a3f853608b5cf7dcb0cfa7f60a09f56dfc1b887753c837eb99af0774e0777c212c34f0'
