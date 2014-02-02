85df15479f6dbb178e9dafb99d1fbd8eab3ce31e
carolann <carolanncan@gmail.com>
2014-02-01 20:50:51 -0800

    Add gem slim-rails Change file naming for slim Add new styles for new.html.slim Add visual-grid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def new_letter
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 LetterWritingRails::Application.routes.draw do
-  root to: 'homes#index'
+  get "static_pages/home"
+  get "static_pages/new_letter"
+  root 'static_pages#home'
 
   get "static_pages/new"
-  get "static_pages/edit"
-  get "static_pages/new_send"
-  get "static_pages/show"
 end
