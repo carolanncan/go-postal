3e92b0d741dc1fface0ee555d0e5a0d4a634d85f
carolanncan <carolanncan@gmail.com>
2014-03-23 23:58:58 -0700

    Add sliding menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= sliding_menu.js
 //= require jquery
 //= require jquery_ujs
 //= require jquery.bettertabs.min
